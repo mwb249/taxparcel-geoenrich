bed7486b21cc9a161992aa016371163e8bb59de6
Mark Brown <marcopolo24124@gmail.com>
2020-06-10 11:20:48 -0400

    Created requirements.txt. Added table field mappings. Fixed reprojection issue.

--- tpgeoenrich.py
+++ tpgeoenrich.py
@@ -5,12 +5,13 @@ published to the geodatabase specified in the config.yml file.
 """
 
 import os
-import yaml
-import requests
-import tempfile
 import shutil
+import tempfile
 from zipfile import ZipFile
 import arcpy
+import requests
+import yaml
+from arcgis.gis import GIS
 
 # Set current working directory
 cwd = os.getcwd()
@@ -20,27 +21,46 @@ with open(cwd + '/config.yml', 'r') as yaml_file:
     cfg = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
 # Set variables for field corrections
-field_lst = [['REVISIONDA', 'REVISIONDATE', 'Revision Date'],
-             ['CVTTAXCODE', 'CVTTAXCODE', 'CVT Tax Code'],
-             ['CVTTAXDESC', 'CVTTAXDESCRIPTION', 'CVT Name'],
-             ['SITEADDRES', 'SITEADDRESS', 'Site Address'],
-             ['SITECITY', 'SITECITY', 'Site City'],
-             ['SITESTATE', 'SITESTATE', 'Site State'],
-             ['SITEZIP5', 'SITEZIP5', 'Site Zip 5'],
-             ['ASSESSEDVA', 'ASSESSEDVALUE', 'Assessed Value'],
-             ['TAXABLEVAL', 'TAXABLEVALUE', 'Taxable Value'],
-             ['NUM_BEDS', 'NUM_BEDS', 'Number of Bedrooms'],
-             ['NUM_BATHS', 'NUM_BATHS', 'Number of Bathrooms'],
-             ['STRUCTURE_', 'STRUCTURE_DESC', 'Structure Type'],
-             ['LIVING_ARE', 'LIVING_AREA_SQFT', 'living Area']]
+field_lst_fc = [['REVISIONDA', 'REVISIONDATE', 'Revision Date'],
+                ['CVTTAXCODE', 'CVTTAXCODE', 'CVT Tax Code'],
+                ['CVTTAXDESC', 'CVTTAXDESCRIPTION', 'CVT Name'],
+                ['SITEADDRES', 'SITEADDRESS', 'Site Address'],
+                ['SITECITY', 'SITECITY', 'Site City'],
+                ['SITESTATE', 'SITESTATE', 'Site State'],
+                ['SITEZIP5', 'SITEZIP5', 'Site Zip 5'],
+                ['ASSESSEDVA', 'ASSESSEDVALUE', 'Assessed Value'],
+                ['TAXABLEVAL', 'TAXABLEVALUE', 'Taxable Value'],
+                ['NUM_BEDS', 'NUM_BEDS', 'Number of Bedrooms'],
+                ['NUM_BATHS', 'NUM_BATHS', 'Number of Bathrooms'],
+                ['STRUCTURE_', 'STRUCTURE_DESC', 'Structure Type'],
+                ['LIVING_ARE', 'LIVING_AREA_SQFT', 'living Area']]
 
-dropFields = ['OBJECTID', 'OBJECTID_1', 'Shapearea', 'Shapelen']
+dropFields_fc = ['OBJECTID', 'OBJECTID_1', 'Shapearea', 'Shapelen']
+
+field_lst_tbl = [['PIN', 'TEXT', 'PIN', 10, None],
+                 ['pnum', 'TEXT', 'Pnum', 20, 'pnum'],
+                 ['neighborhoodcode', 'TEXT', 'Neighborhood Code', 5, 'ecftbl'],
+                 ['classcode', 'TEXT', 'Class Code', 3, 'propclass'],
+                 ['schooltaxcode', 'TEXT', 'School Tax Code', 5, 'schooldist'],
+                 ['relatedpnum', 'TEXT', 'Related Pnum', 20, 'relatedpnum'],
+                 ['propstreetcombined', 'TEXT', 'Property Street Combined', 350, 'propstreetcombined'],
+                 ['propaddrnum', 'DOUBLE', 'Property Address Number', None, 'propaddrnum'],
+                 ['propstreetname', 'TEXT', 'Property Street Name', 350, 'propstreetname'],
+                 ['propzip', 'TEXT', 'Property Zip Code', 10, 'propzip'],
+                 ['ownername1', 'TEXT', 'Owner Name 1', 180, 'ownername1'],
+                 ['ownername2', 'TEXT', 'Owner Name 2', 180, 'ownername2'],
+                 ['ownerstreetaddr', 'TEXT', 'Owner Street Address', 350, 'ownerstreetaddr'],
+                 ['ownercity', 'TEXT', 'Owner City', 70, 'ownercity'],
+                 ['ownerstate', 'TEXT', 'Owner State', 2, 'ownerstate'],
+                 ['ownerzip', 'TEXT', 'Owner Zip', 10, 'ownerzip'],
+                 ['ownercountry', 'TEXT', 'Owner Country', 90, 'ownercountry'],
+                 ['exemptcode', 'TEXT', 'Taxable Status', 50, 'exemptcode']]
 
 
 def formatpin(pnum, relatedpnum):
     """Formats a Parcel ID Number (PIN) from a BS&A PNUM."""
     try:
-        if relatedpnum is None:
+        if relatedpnum is None or relatedpnum.startswith('70-15-17-6'):
             p = pnum
         else:
             p = relatedpnum
@@ -91,8 +111,54 @@ def cleanup(directory):
     return 'All temporary files deleted.'
 
 
+def pushtogdb(final_lyr):
+    """Copies the finalized layer to a geodatabase. The feature class will be reprojected, if specified in the config
+    file. If a feature service is referencing the feature class, it will be stopped prior to copying features and
+    restarted afterwards. """
+
+    # Create connection to ArcGIS Portal
+    print('Establishing connection to ArcGIS Enterprise Portal...')
+    gis = GIS(profile=cfg['webgis']['profile'])
+
+    # List federated GIS servers, set variable for first server in list
+    print('Finding federated GIS servers...')
+    gis_server = gis.admin.servers.list()[0]
+
+    # List all services for specified folder
+    services = gis_server.services.list(cfg['webgis']['serv_folder'])
+
+    # Stop feature service
+    service = None
+    serv_stop = False
+    for serv in services:
+        if serv.properties.serviceName == cfg['webgis']['serv_name']:
+            print('Stopping feature service...')
+            service = serv
+            serv_stop = service.stop()
+            print('Feature service stopped...')
+
+    # Change environment workspace
+    arcpy.env.workspace = cfg['gis_env']['workspace']
+
+    # Clear environment workspace cache
+    arcpy.ClearWorkspaceCache_management()
+
+    # Output final_lyr to enterprise geodatabase feature class
+    print('Copying features to geodatabase...')
+    arcpy.CopyFeatures_management(final_lyr, cfg['gis_env']['out_fc_name'])
+
+    # Clear environment workspace cache
+    arcpy.ClearWorkspaceCache_management()
+
+    # Restart feature service
+    if serv_stop:
+        print('Starting feature service...')
+        service.start()
+
+
 def geoenrich(directory):
-    """Takes zipped response from website, joins BS&A table data, then copies to a geodatabase feature class."""
+    """Intakes a tax parcel shapefile, modifies fields, reprojects, then joins to BS&A table data. The final layer is
+    copied to a geodatabase feature class."""
     # Set initial environment workspace
     print('Setting environment workspace and settings...')
     arcpy.env.workspace = directory
@@ -102,7 +168,7 @@ def geoenrich(directory):
     arcpy.env.overwriteOutput = cfg['gis_env']['overwrite_output']
 
     # Set variable to name of shapefile
-    print('finding shapefile...')
+    print('Finding shapefile...')
     shp_name = shpfilename(directory)
 
     # Make a layer from the shapefile
@@ -113,9 +179,9 @@ def geoenrich(directory):
     cvt_list_len = len(cfg['cvt_codes'])
     if cvt_list_len > 1:
         cvt_list_tup = tuple(cfg['cvt_codes'])
-        sql = "CVTTAXCODE IN {0}".format(cvt_list_tup)
+        sql = "CVTTAXCODE IN {}".format(cvt_list_tup)
     elif cvt_list_len == 1:
-        sql = "CVTTAXCODE = '{0}'".format(cfg['cvt_codes'][0])
+        sql = "CVTTAXCODE = '{}'".format(cfg['cvt_codes'][0])
     else:
         sql = 'CVTTAXCODE = ALL'
 
@@ -138,47 +204,21 @@ def geoenrich(directory):
     # Change environment workspace
     arcpy.env.workspace = gdb_path
 
-    # Correct field names
-    for f in field_lst:
-        arcpy.AlterField_management('parcel_fc', f[0], f[1], f[2])
+    # Correct field names (feature class)
+    for field_prop in field_lst_fc:
+        arcpy.AlterField_management('parcel_fc', field_prop[0], field_prop[1], field_prop[2])
 
-    # Delete unnecessary fields
-    arcpy.DeleteField_management('parcel_fc', dropFields)
-
-    # Make Parcel layer
-    arcpy.MakeFeatureLayer_management('parcel_fc', 'parcel_lyr')
-
-    # Convert CSV to GDB table
-    print('Finding table...')
-    arcpy.TableToTable_conversion(cfg['csv_uri'], gdb_path, 'bsa_export')
-
-    # Add field to bsa_export table
-    arcpy.AddField_management('bsa_export', 'PIN', "TEXT", field_alias='PIN', field_length=10,
-                              field_is_nullable="NULLABLE")
-
-    expression = 'formatpin(!PNUM!, !RELATEDPNUM!)'
-
-    # Calculate field
-    arcpy.CalculateField_management('bsa_export', 'PIN', expression, 'PYTHON3')
-
-    # Join parcel_lyr to bsa_export table
-    print('Joining table to parcel layer...')
-    arcpy.AddJoin_management('parcel_lyr', 'PIN', 'bsa_export', 'PIN')
-
-    # Make a new layer with joined data
-    arcpy.MakeFeatureLayer_management('parcel_lyr', 'final_lyr')
-
-    # Change environment workspace
-    arcpy.env.workspace = cfg['gis_env']['workspace']
+    # Delete unnecessary fields (feature class)
+    arcpy.DeleteField_management('parcel_fc', dropFields_fc)
 
     # Modify projection if necessary
     print('Assessing coordinate system...')
-    in_spatial_ref = arcpy.Describe('final_lyr').spatialReference
+    in_spatial_ref = arcpy.Describe('parcel_fc').spatialReference
     out_spatial_ref = arcpy.SpatialReference(cfg['gis_env']['out_fc_proj'])
-    if in_spatial_ref == 'Unknown':
+    if in_spatial_ref.name == 'Unknown':
         change_proj = False
         print('Could not change projection due to undefined input coordinate system')
-    elif in_spatial_ref == out_spatial_ref:
+    elif in_spatial_ref.name == out_spatial_ref.name:
         change_proj = False
         print('Input and output coordinate systems are the same')
     else:
@@ -187,16 +227,65 @@ def geoenrich(directory):
 
     # Output final_lyr to enterprise geodatabase feature class
     print('Copying features to geodatabase feature class...')
-    if not change_proj:
-        arcpy.CopyFeatures_management('final_lyr', cfg['gis_env']['out_fc_name'])
+    if change_proj:
+        print('Changing projection, making feature layer...')
+        arcpy.Project_management('parcel_fc', 'parcel_fc_proj', out_spatial_ref)
+        arcpy.MakeFeatureLayer_management('parcel_fc_proj', 'parcel_lyr')
     else:
-        arcpy.Project_management('final_lyr', cfg['gis_env']['out_fc_name'], out_spatial_ref)
+        print('Making feature layer...')
+        arcpy.MakeFeatureLayer_management('parcel_fc', 'parcel_lyr')
+
+    # Convert CSV to GDB table
+    print('Finding table...')
+    arcpy.TableToTable_conversion(cfg['csv_uri'], gdb_path, 'bsa_export')
+
+    # Create empty table to load bsa_export data
+    arcpy.CreateTable_management(gdb_path, 'join_table')
+
+    # Add fields from field_lst_tbl
+    for field in field_lst_tbl:
+        if field[1] == 'TEXT':
+            arcpy.AddField_management('join_table', field[0], field[1], field_alias=field[2], field_length=field[3])
+        else:
+            arcpy.AddField_management('join_table', field[0], field[1], field_alias=field[2])
+
+    # Create FieldMappings object to manage merge output fields
+    field_mappings = arcpy.FieldMappings()
+
+    # Add the target table to the field mappings class to set the schema
+    field_mappings.addTable('join_table')
+
+    # Map fields from bsa_export table
+    for field in field_lst_tbl:
+        if field[4] is not None:
+            fld_map = arcpy.FieldMap()
+            fld_map.addInputField('bsa_export', field[4])
+            # Set name of new output field
+            field_name = fld_map.outputField
+            field_name.name, field_name.type, field_name.aliasName = field[0], field[1], field[2]
+            fld_map.outputField = field_name
+            # Add output field to field mappings object
+            field_mappings.addFieldMap(fld_map)
+
+    # Append the bsa_export data into the join_table
+    arcpy.Append_management('bsa_export', 'join_table', schema_type='NO_TEST', field_mapping=field_mappings)
+
+    expression = 'formatpin(!PNUM!, !RELATEDPNUM!)'
+
+    # Calculate field
+    arcpy.CalculateField_management('join_table', 'PIN', expression, 'PYTHON3')
+
+    # Join parcel_lyr to bsa_export table
+    print('Joining table to parcel layer...')
+    arcpy.AddJoin_management('parcel_lyr', 'PIN', 'join_table', 'PIN')
+
+    # Modify coordinate system and copy to geodatabase
+    pushtogdb('parcel_lyr')
 
     # Delete temporary layers and temporary file geodatabase
     print('Deleting temporary layers...')
-    arcpy.Delete_management('parcel_lyr')
-    arcpy.Delete_management('bsa_export')
-    arcpy.Delete_management('final_lyr')
+
+    arcpy.Delete_management(r"'parcel_lyr';'bsa_export';'join_table'")
     arcpy.Delete_management(gdb_path)
 
     # Clear environment workspace cache
